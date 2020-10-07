5fd1e3aacde08ec2b1725087e2135e83c6fe22e8
SoraInoue <66369807+SoraInoue@users.noreply.github.com>
2020-10-07 20:14:30 +0900

    10/7

---
+++ app/controllers/mains_controller.rb
@@ -0,0 +1,7 @@
+class MainsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'calendars#index'
+  resources :users, only: [:edit, :update]
+  resources :mains, only: [:new, :create]
 end
