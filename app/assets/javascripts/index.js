c27e18dd7b97f50908ab82b435efb909e1095eeb
SoraInoue <66369807+SoraInoue@users.noreply.github.com>
2020-10-12 21:31:38 +0900

    変更

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,13 +1,16 @@
 $(document).on('turbolinks:load', function(){
+  // クリック時side-memoの追加
   $(document).on('click','.sidememo__bar__plus', function(){
-    let sidememoLength = $('#memo-box').children('li').length;
-    
-    $("#memo-input").before(`<li class="side-memo" id="write-memo${sidememoLength}">
-                              <div class="memo-preview">
+
+    let labelLength = $("#memo-input>label").data('label-id');
+    console.log(`${labelLength}`)
+
+    $("#memo-input").before(`<li class="side-memo" id="write-memo${labelLength}" date-smemo-id="${labelLength}">
+                              <div class="memo-preview${labelLength}">
                                 <div class="memo-preview__bar">
                                   <input class="memo-preview__bar__title" type="text" >
-                                  <div class="memo-preview__bar__times" date-id="${sidememoLength}">
-                                    <i class="fas fa-times"></i>
+                                  <div class="memo-preview__bar__times" date-smemo-id="${labelLength}">
+                                    <i class="fas fa-times" ></i>
                                   </div>
                                 </div>
                                 <div class="memo-preview__text">
@@ -15,6 +18,11 @@ $(document).on('turbolinks:load', function(){
                                 </div>
                               </div>
                             </li>`)
-    
+  });
+  
+  $('#memo-box').on('click','.memo-preview__bar__times', function(){
+    let targetSmemoId = $(this).data(`smemo-id`);
+    console.log(`${targetSmemoId}`)
+    $(`#write-memo${targetSmemoId}`).remove();
   });
 });
