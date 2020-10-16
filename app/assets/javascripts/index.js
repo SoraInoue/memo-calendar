aac041d870a24214942ec3d871c59fd1a48c2740
SoraInoue <66369807+SoraInoue@users.noreply.github.com>
2020-10-16 16:40:44 +0900

    サイドメモ削除機能追加

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,28 +1,30 @@
 $(document).on('turbolinks:load', function(){
   // クリック時side-memoの追加
+  var labelLength = 0;
   $(document).on('click','.sidememo__bar__plus', function(){
-
-    let labelLength = $("#memo-input>label").data('label-id');
-    console.log(`${labelLength}`)
-
-    $("#memo-input").before(`<li class="side-memo" id="write-memo${labelLength}" date-smemo-id="${labelLength}">
-                              <div class="memo-preview${labelLength}">
-                                <div class="memo-preview__bar">
-                                  <input class="memo-preview__bar__title" type="text" >
-                                  <div class="memo-preview__bar__times" date-smemo-id="${labelLength}">
-                                    <i class="fas fa-times" ></i>
+    
+    let memoLength = $('#memo-box').children('li').length;
+    console.log(memoLength)
+    if (memoLength < 5) {
+      $("#memo-input").before(`<li class="side-memo" id="write-memo${labelLength}" date-smemo-id="${labelLength}">
+                                <div class="memo-preview${labelLength}">
+                                  <div class="memo-preview__bar">
+                                    <input class="memo-preview__bar__title" type="text" >
+                                    <div class="memo-preview__bar__times" data-smemo="${labelLength}">
+                                      <i class="fas fa-times" ></i>
+                                    </div>
+                                  </div>
+                                  <div class="memo-preview__text">
+                                    <textarea class="memo-preview__text__main"></textarea>
                                   </div>
                                 </div>
-                                <div class="memo-preview__text">
-                                  <textarea class="memo-preview__text__main"></textarea>
-                                </div>
-                              </div>
-                            </li>`)
+                              </li>`)
+      labelLength += 1;
+    }
   });
   
   $('#memo-box').on('click','.memo-preview__bar__times', function(){
-    let targetSmemoId = $(this).data(`smemo-id`);
-    console.log(`${targetSmemoId}`)
+    let targetSmemoId = $(this).data('smemo');
     $(`#write-memo${targetSmemoId}`).remove();
   });
 });
